5a927fd864a367f87160ed9175dd6cd7e44bd583
Ron Cohen <ronc@moveoboost.com>
2022-04-13 14:13:26 +0300

    finished basic restaurant page functionality

--- epicure/src/pages/RestaurantsPage.tsx
+++ epicure/src/pages/RestaurantsPage.tsx
@@ -15,19 +15,31 @@ const RestaurantsPage = () => {
     getAllRes();
   }, []);
   const popularHandler = () => {
-    const popular = restaurants?.filter((restaurant) => {
+    const popular = all?.filter((restaurant) => {
       return restaurant.rating > 8;
     });
     popular ? setRestaurants(popular) : setRestaurants(null);
+  };
+
+  const openHandler = () => {
+    const time = new Date(Date.now());
+
+    const open = restaurants?.filter((restaurant) => {
+      return (
+        restaurant.closingTime > time.getHours() &&
+        restaurant.openTime <= time.getHours()
+      );
+    });
+    open ? setRestaurants(open) : setRestaurants(null);
   };
   return (
     <>
       <Header />
       <div>
         <button onClick={() => setRestaurants(all)}>All</button>
-        <button>New</button>
+        <button onClick={popularHandler}>New</button>
         <button onClick={popularHandler}>Most Popular</button>
-        <button>Open Now</button>
+        <button onClick={openHandler}>Open Now</button>
       </div>
       {restaurants?.map((restaurant) => {
         return <img src={restaurant.picture} alt={restaurant.name} />;
